d68b0e0a24d4be16f79ddfcac179b844ab2e419d
dlh <22029832+dlh996@users.noreply.github.com>
2025-02-28 08:47:50 +0800

    fix: update

--- src/apps/com.tencent.mm.ts
+++ src/apps/com.tencent.mm.ts
@@ -149,7 +149,7 @@ export default defineGkdApp({
             'com.tencent.mm.plugin.webwx.ui.ExtDeviceWXLoginUI',
             'com.tencent.mm.ui.LauncherUI',
           ],
-          matches: 'TextView[text*="登录"] - Button[text*="登录"]',
+          matches: 'TextView[text*="登录"] - Button',
           snapshotUrls: [
             'https://i.gkd.li/i/13522625',
             'https://i.gkd.li/i/13522577',
